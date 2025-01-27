33c1e1b98f92119b1d687f275d772b352a16ff3f
RodziNa <pototzkaja.n@gmail.com>
2025-01-28 00:38:19 +0200

    try

--- backend/index.js
+++ backend/index.js
@@ -13,11 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "build")));
 
-// Redirect all other requests to index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 app.use("/api/apartments", apartmentRoutes);
 
 // Connect to MongoDB
