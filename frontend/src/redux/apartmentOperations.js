b917feb08b78e6b736a6ff3bfb8234938c0d4585
RodziNa <pototzkaja.n@gmail.com>
2025-01-27 17:46:08 +0200

    fix package

--- frontend/src/redux/apartmentOperations.js
+++ frontend/src/redux/apartmentOperations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/apartments";
+const API_URL = "https://flat-for-rent-back.onrender.com";
 
 export const fetchApartments = createAsyncThunk(
   "apartments/fetchApartments",
